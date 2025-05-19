f597ac8bcac12d4d9c0ea74a032044a640b3fad5
Jesse Ikemefuna <jesseikemefuna@gmail.com>
2025-05-19 09:46:28 +0100

    minor onboarding changes

--- lib/features/onboarding/presentation/screens/onboarding_screen1.dart
+++ lib/features/onboarding/presentation/screens/onboarding_screen1.dart
@@ -12,60 +12,56 @@ class OnboardingScreen1 extends StatelessWidget {
     // RETURNED SCAFFOLD
     return Scaffold(
       // Image
-      body: Stack(
-        children: [
-          Container(
-            height: screenHeight(context),
-            width: screenWidth(context),
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                filterQuality: FilterQuality.high,
-                image: AssetImage(
-                  'images/OnboardingImages/onboarding_image1.jpg',
-                ),
-              ),
+      body: Container(
+        height: screenHeight(context),
+        width: screenWidth(context),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            filterQuality: FilterQuality.high,
+            image: AssetImage(
+              'images/OnboardingImages/onboarding_image1.jpg',
             ),
+          ),
+        ),
 
-            // Black gradient
-            child: Container(
-              alignment: const Alignment(0, 1),
-              child: OnboardBottomGradient(
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      // Title
-                      Text(
-                        'Welcome To Legwork!',
-                        style: context.headingSm?.copyWith(
-                          color: context.colorScheme.surface,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-
-                      // Subtitle
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 30.0,
-                          vertical: 10.0,
-                        ),
-                        child: Text(
-                          'Empowering Nigerian dancers: Join the platform that puts your talents to work',
-                          textAlign: TextAlign.center,
-                          style: context.textXl?.copyWith(
-                            color: context.colorScheme.surface,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      )
-                    ],
+        // Black gradient
+        child: Container(
+          alignment: const Alignment(0, 1),
+          child: OnboardBottomGradient(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Title
+                  Text(
+                    'Welcome To Legwork!',
+                    style: context.headingSm?.copyWith(
+                      color: context.colorScheme.surface,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
+
+                  // Subtitle
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 10.0,
+                    ),
+                    child: Text(
+                      'Empowering Nigerian dancers: Join the platform that puts your talents to work',
+                      textAlign: TextAlign.center,
+                      style: context.textXl?.copyWith(
+                        color: context.colorScheme.surface,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
