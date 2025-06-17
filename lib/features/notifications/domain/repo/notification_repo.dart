5b108ca57fda937dcd7dfcc150566772449dcbbf
Jesse Ikemefuna <jesseikemefuna@gmail.com>
2025-06-17 12:18:51 +0100

    .

--- lib/features/notifications/domain/repo/notification_repo.dart
+++ lib/features/notifications/domain/repo/notification_repo.dart
@@ -3,7 +3,5 @@ import 'package:legwork/features/notifications/domain/entities/notif_entity.dart
 abstract class NotificationRepo {
   Future<String?> getDeviceToken();
 
-  Future<void> sendNotification({
-    required NotifEntity notif,
-  });
+  Future<void> sendNotification({required NotifEntity notif});
 }
